6c445dfa239fe1af45f994dc279c60548b46a6d4
carlos <carloskip25@gmail.com>
2025-03-23 16:11:13 +0300

    code grade

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// Set up a DOMContentLoaded event listener
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the paragraph element with id="text"
+    const textElement = document.getElementById('text');
+    
+    // Change the text content of the element
+    textElement.textContent = 'This is really cool!';
+  });
+  
